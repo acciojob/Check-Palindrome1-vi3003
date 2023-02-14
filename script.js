4020c7a307405d7a7d926babd24ed52ed9766774
vidhi jain <117736922+vi3003@users.noreply.github.com>
2023-02-14 21:37:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 // complete the given function
 
 function palindrome(str){
-
+str = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+	if(str.length == 0){
+		return true
+	}else{
+		let i = 0
+	let j = str.length - 1
+	while(i<j){
+		if(str[i] != str[j]){
+			return false
+		}else{
+			i++;
+			j--;
+		}
+	}
+	return true
+	}
 }
 module.exports = palindrome
